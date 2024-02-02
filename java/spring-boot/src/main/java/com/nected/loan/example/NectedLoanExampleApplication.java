7362d20813af937495674559d162329b4b5bf40c
Mrigesh Pokhrel <mrigesh@nected.ai>
2024-02-02 14:24:57 +0530

    chore: code cleanup
    Signed-off-by: Mrigesh Pokhrel <mrigesh@nected.ai>

--- java/spring-boot/src/main/java/com/nected/loan/example/NectedLoanExampleApplication.java
+++ java/spring-boot/src/main/java/com/nected/loan/example/NectedLoanExampleApplication.java
@@ -1,7 +1,5 @@
 package com.nected.loan.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
